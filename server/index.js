9a4d5f1d614c88622816c9539bda8e42c2c2d889
Gregory Kocharyan <groyreg@gmail.com>
2024-03-16 22:54:47 +0300

    backend add mongo connect

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
 app.use(
   cors({
     origin: ['https://gustavogaviria17.github.io', 'http://localhost:5175'], // Разрешенные источники
@@ -19,8 +21,11 @@ app.get('/', async (req, res) => {
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.MONGO_URL);
     app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
-  } catch (e) {}
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 start();
