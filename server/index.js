bbbef34d62cee608457ed16be1caa684f9a33e06
Gregory Kocharyan <groyreg@gmail.com>
2024-03-16 22:05:56 +0300

    add server config init 2

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get('/test', async (req, res) => {
-  res.send('dfsdfsd');
+app.get('/', async (req, res) => {
+  res.send('Hello world');
 });
 
 const start = async () => {
